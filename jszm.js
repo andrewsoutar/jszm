7e4a83ff6a733aaa0ac1b7dae53326cfc97d155f
Andrew Soutar <user@andrewsoutar.com>
2020-05-06 00:33:31 -0400

    getText: remove some unused variables and simplify a bit

--- jszm.js
+++ jszm.js
@@ -207,25 +207,19 @@ JSZM.prototype = {
         if (encodedChar == 0) {
           output += " ";
         } else if (encodedChar < 4) {
-          temporaryShift = 5;
           auxParsingState = (encodedChar - 1) * 32;
-          const encodedChar2 = getNextEncodedChar();
-          output += this.getText(this.getu(this.fwords + (auxParsingState + encodedChar2) * 2) * 2);
+          output += this.getText(this.getu(this.fwords + (auxParsingState + getNextEncodedChar()) * 2) * 2);
           temporaryShift = permanentShift;
         } else if (encodedChar < 6) {
-          if (!temporaryShift)
+          if (temporaryShift === 0)
             temporaryShift = encodedChar - 3;
           else if (temporaryShift == encodedChar - 3)
             permanentShift = temporaryShift;
           else
             permanentShift = temporaryShift = 0;
         } else if (encodedChar == 6 && temporaryShift == 2) {
-          temporaryShift = 3;
-          const encodedChar2 = getNextEncodedChar();
-          auxParsingState = encodedChar2 << 5;
-          temporaryShift = 4;
-          const encodedChar3 = getNextEncodedChar();
-          auxParsingState += encodedChar3;
+          auxParsingState = getNextEncodedChar() << 5;
+          auxParsingState += getNextEncodedChar();
           if (auxParsingState == 13) {
             output += "\n";
           } else if (auxParsingState) {
